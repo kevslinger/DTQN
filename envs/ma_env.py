a859be17df763fc640ede6b2bf1c7bffc65620ac
Xueguang <lu.xue@northeastern.edu>
2022-12-02 12:35:46 -0600

    removed marl stuff

--- envs/ma_env.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC
-from gym import Env, Space
-
-
-class MultiAgentEnv(Env, ABC):
-    def __init__(self):
-        self.n_agents = 0
-        self.observation_space = Space()
-        self.action_space = Space()
